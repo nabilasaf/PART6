b1f0fd1945288232566ae5a12639b330dd064ccb
nabilasaf <nabilasafitri861@gmail.com>
2024-04-02 14:34:55 +0700

    memasukkan struktur mahasiswa

--- Part_1.cpp
+++ Part_1.cpp
@@ -10,17 +10,5 @@ struct Mahasiswa {
 
 int main()
 {
-    Mahasiswa mhs;
-    cout << "Nomor Mahasiswa : ";
-    cin >> mhs.nim;
-    cout << "Nama Mahasiswa : ";
-    cin >> mhs.alamat;
-    cout << "Umur Mahasiswa : ";
-    cin >> mhs.umur;
-
-    cout << endl;
-    cout << "\n nim : " << mhs.nim;
-    cout << "\n nama : " << mhs.nama;
-    cout << "\n alamat : " << mhs.alamat;
-    cout << "\n umur : " << mhs.umur;
+    
 };
